275d959ae12e30538cba1143ae79d13d47b7489f
Malisa Ncube <malisa.ncube@gmail.com>
2017-06-15 12:15:27 +0300

    - A bunch of renames - Added unittests file

---
+++ src/unit_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import sol1, sol2
+
+class TestAggregations(unittest.TestCase):
+    """Aggregation Tests"""
+    def sum_of_functional_water_points(self):
+        """Verify the number of waterpoints"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
