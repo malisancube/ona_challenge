74f94e667159b202bc0e6a0ab9744a68276df360
Malisa Ncube <malisa.ncube@gmail.com>
2017-06-15 15:47:34 +0300

    Calclucate function created

--- src/sol1.py
+++ src/sol1.py
@@ -1,11 +1,23 @@
 import pandas as pd
 
-df = pd.read_json('https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json')
-df = df[['water_functioning', 'communities_villages']]
-df = df[df['water_functioning'] == 'yes'].groupby(['communities_villages']).count()
-df['ranking'] = df['water_functioning'].rank(ascending=False)
-df.to_json(orient='index')
+def calculate(url):
+    
+    df = pd.read_json(url)
+    df = df[['water_functioning', 'communities_villages']]
+    df = df.groupby(['communities_villages', 'water_functioning']).size().unstack()
+    df = df.fillna(0)
 
-total_functional = df['water_functioning'].sum()
+    df['total'] = df['yes'] + df['no']
+    df['fault_percentage'] = df['no'] / df['total']
+    df['ranking'] = df['fault_percentage'].rank(ascending=False)
 
-#http://www.shanelynn.ie/summarising-aggregation-and-grouping-data-in-python-pandas/
+    total_functional = df['yes'].sum()
+    df = df.rename(columns={'yes': 'functional', 'no':'non_functional'})
+    df = df[['functional', 'non_functional', 'total', 'ranking']]
+    records = df.to_json(orient='index')
+    result = '{"number_functional": %s, %s}' % (total_functional, records)
+    return result
+
+
+print calculate('https://raw.githubusercontent.com/onaio/ona-tech/master/data/water_points.json')
+
